271f5b50daaf6da405ef970d5c0104b9be1a2e20
Moulaye Keita <moulaye.c.keita@gmail.com>
2019-04-29 16:02:21 -0700

    get the query params to work

--- index.js
+++ index.js
@@ -51,10 +51,11 @@ app.post("/new-message", async (req, res) => {
 
 app.post("/positive-messages", async (req, res) => {
 
-  var user_id = req.body.user_id;
-  if (user_id) {
+  var user_id = (req.body.queries != null && req.body.queries[0] != null) ? req.body.queries[0].value : null; // the first query if for the user_id
+  console.log("here: ", req.body.queries[0].value)
+  if (user_id != null) {
      console.log("getting existing user (", user_id + ")'s positive messages from db")
-     let response = await getPositiveMessagesForOneUser(user_id);
+     let response = await getPositiveMessagesForOneUser(req.body);
      if (response.success)
       res.json({ success: true, resource: response.resource });
      else
@@ -65,25 +66,6 @@ app.post("/positive-messages", async (req, res) => {
     console.log("getting all users positive messages from db")
   }
 
-});
-
-app.post("/sentiment", (req, res) => {
-  // console.log("req:", req.param);
-  console.log("req query:", req.query);
-  var phrase = req.query.phrase;
-  console.log("phrase:", phrase);
-  const client = new Wit({
-    accessToken: process.env.WIT_TOKEN,
-    logger: new log.Logger(log.DEBUG) // optional
-  });
-
-  client
-    .message(phrase)
-    .then(data => {
-      console.log("Yay, got Wit.ai response: " + JSON.stringify(data));
-    })
-    .catch(console.error);
-  // res.json({ success: true, resource: response });
 });
 
 var saveUserInfo = async function(req, doc, userRef) {
@@ -126,7 +108,6 @@ var saveUserInfo = async function(req, doc, userRef) {
 
   return new Promise(async (res, rej) => {
     let response = await analyzeSentiment(message, messagesRef);
-    //console.log("update db object: " + response.entities.sentiment[0].value);
     if (response.success)
       res({ success: true, resource: response.resource });
     else
@@ -151,11 +132,13 @@ var analyzeSentiment = function(message, messagesRef) {
       .message(message.content)
       .then(data => {
         //console.log("Yay, got Wit.ai response: " + JSON.stringify(data));
-        console.log("update db object: " + data.entities.sentiment[0].value);
-        db.collection("messages")
-        .where("user_id", "==", message.user_id)
-        .where("timestamp", "==", message.timestamp)
-        .get()
+        console.log("update db object");
+
+        let query = db.collection("messages")
+        for (let q of body.queries) {
+           query.where(q.field, q.operation, q.value)
+        }
+        query.get()
         .then(function(querySnapshot) {
             querySnapshot.forEach(function(doc) {
                 console.log(doc.id, " => ", doc.data());
@@ -170,9 +153,7 @@ var analyzeSentiment = function(message, messagesRef) {
                 })
                 .then(() => {
                   // update complete, let's return the data
-                  messagesRef
-                  .where("user_id", "==", message.user_id)
-                  .where("timestamp", "==", message.timestamp)
+                  query
                   .get()
                   .then(function(querySnapshot) {
                     querySnapshot.forEach(function(theDoc) {
@@ -182,10 +163,10 @@ var analyzeSentiment = function(message, messagesRef) {
                   });
                 })
             });
-       });
-        }).catch(function(error) {
-            res({ success: false })
         });
+      }).catch(function(error) {
+          res({ success: false })
+      });
 
       // res.json({ success: true, resource: data });
   })
@@ -194,13 +175,15 @@ var analyzeSentiment = function(message, messagesRef) {
   });
 };
 
-var getPositiveMessagesForOneUser = async function(user_id) {
+var getPositiveMessagesForOneUser = async function(body) {
   return new Promise((res, rej) => {
       var positiveSentiments = []
-      db.collection("messages")
-      .where("user_id", "==", user_id)
-      .where("sentiment", "==", "positive")
-      .get()
+
+      let query = db.collection("messages")
+      for (let q of body.queries) {
+         query.where(q.field, q.operator, q.value)
+      }
+      query.get()
       .then(function(querySnapshot) {
           querySnapshot.forEach(function(doc) {
               console.log(doc.id, " => ", doc.data());
