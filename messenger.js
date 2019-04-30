fdbc6208bcd9a063f626de31a0ee2a6e5cde219a
Alexis Montoya <agl125@hotmail.com>
2019-04-29 21:39:44 -0700

    webhook fix on parse messaging

--- messenger.js
+++ messenger.js
@@ -75,16 +75,18 @@ router.post("/webhook", (req, res) => {
   console.log(JSON.stringify(body));
   if (body.object === "page") {
     body.entry.forEach(function(entry) {
-      let webhook_event = entry.messaging[0];
-      console.log(webhook_event);
+      if (entry.messaging) {
+        let webhook_event = entry.messaging[0];
+        console.log(webhook_event);
 
-      let sender_psid = webhook_event.sender.id;
-      console.log("Sender PSID: " + sender_psid);
+        let sender_psid = webhook_event.sender.id;
+        console.log("Sender PSID: " + sender_psid);
 
-      if (webhook_event.message) {
-        handleMessage(sender_psid, webhook_event);
-      } else if (webhook_event.postback) {
-        handlePostback(sender_psid, webhook_event);
+        if (webhook_event.message) {
+          handleMessage(sender_psid, webhook_event);
+        } else if (webhook_event.postback) {
+          handlePostback(sender_psid, webhook_event);
+        }
       }
     });
 
